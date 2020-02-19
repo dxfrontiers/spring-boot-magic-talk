003c047a22f9ee66c28416e8360dccc7adfb3da2
Francois Fernandes <francois.fernandes@digitalfrontiers.de>
2020-02-20 00:39:48 +0100

    Included configuration properties

--- mina/mina-autoconfigure/src/main/java/de/digitalfrontiers/springboot/mina/autoconfigure/MinaSSHDAutoConfiguration.java
+++ mina/mina-autoconfigure/src/main/java/de/digitalfrontiers/springboot/mina/autoconfigure/MinaSSHDAutoConfiguration.java
@@ -10,22 +10,29 @@ import org.apache.sshd.server.auth.password.PasswordChangeRequiredException;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
 import org.apache.sshd.server.session.ServerSession;
 import org.apache.sshd.server.shell.ShellFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Optional;
+
 @ConditionalOnClass(SshServer.class)
+@EnableConfigurationProperties(SSHDProperties.class)
 @Configuration
 public class MinaSSHDAutoConfiguration {
 
+  @Autowired
+  SSHDProperties properties;
 
   @ConditionalOnMissingBean
   @Bean(initMethod = "start", destroyMethod = "stop")
   public SshServer sshServer(ShellFactory shellFactory) {
     final ServerBuilder builder = ServerBuilder.builder();
     final SshServer server = builder.build();
-    server.setPort(8022);
+    server.setPort(properties.getPort());
 
     server.setKeyboardInteractiveAuthenticator(
         new DefaultKeyboardInteractiveAuthenticator());
@@ -42,7 +49,16 @@ public class MinaSSHDAutoConfiguration {
       @Override
       public boolean authenticate(String username, String password, ServerSession session)
           throws PasswordChangeRequiredException, AsyncAuthException {
-        return "admin".equals(username) && "password".equals(password);
+
+        final Optional<SSHDProperties.UserDetail> userDetail = properties
+            .getUsers()
+            .stream()
+            .filter(details -> username.equals(details.getUsername()))
+            .findFirst();
+
+        return userDetail
+            .map(ud -> password.equals(ud.getPassword()))
+            .orElse(false);
       }
     };
   }
